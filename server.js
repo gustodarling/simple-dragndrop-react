04d278ceb1f77f61cc4be205698e6f63274d16ba
GUSTO <gustodarling@gmail.com>
2019-04-12 17:30:24 -0700

    fix

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express')
 var app = express()
 app.use(express.static(__dirname + '/'))
-app.listen(process.env.PORT || 8080)
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+//////////////////////
+app.listen(port);
