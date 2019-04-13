d43a7d9d5985ce75ecace881cefd7ccad140f1ae
GUSTO <gustodarling@gmail.com>
2019-04-12 17:34:23 -0700

    heroku server

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 var express = require('express')
 var app = express()
 app.use(express.static(__dirname + '/'))
-
-app.get('*', (req, res) =>{
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
-//////////////////////
-app.listen(port);
+app.listen(process.env.PORT || 8080)
